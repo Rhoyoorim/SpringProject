91208b38b294ef2534de1a2615ebbd37c516a8ea
Rhoyoorim <109774361+Rhoyoorim@users.noreply.github.com>
2022-10-21 13:40:54 +0900

    model&controller

--- java/com/son/biz/controller/MyAuthentication.java
+++ java/com/son/biz/controller/MyAuthentication.java
@@ -6,8 +6,8 @@ import javax.mail.PasswordAuthentication;
 public class MyAuthentication extends Authenticator {
 	PasswordAuthentication pa;
 	public MyAuthentication(){
-		String id="sonpill5000@gmail.com"; // 구글계정
-		String pw="hynqgpscfswfdhun"; // 구글 앱번호
+		String id="구글계정"; // 구글계정
+		String pw="구글 앱번호"; // 구글 앱번호
 		
 		pa = new PasswordAuthentication(id, pw);
 	}
